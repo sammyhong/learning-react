9d4cdc8a9829e3aa4cace871bea8e02ea8194b03
sammyHong <samee1@naver.com>
2022-12-07 11:13:59 +0900

    Coin Tracker 만들기

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 // import App from "./App";
-import TodoListApp from "./TodoListApp";
+// import TodoListApp from "./TodoListApp";
+import CoinTrackerApp from "./CoinTrackerApp";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <App />
-  <TodoListApp />
+  // <TodoListApp />
+  <CoinTrackerApp />
 );
